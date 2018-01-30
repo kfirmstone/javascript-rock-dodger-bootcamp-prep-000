1264925dbbffa9e06b4a508a395051814651b1af
Kyle Firmstone <kfirmstonecpt@gmail.com>
2018-01-30 04:04:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -145,7 +145,7 @@ function moveDodgerLeft() {
    window.requestAnimationFrame(function() {
     const left = positionToInteger(DODGER.style.left)
     if (left>0) {
-      DODGER.style.left = `${left - 6}px`
+      DODGER.style.left = `${left - 4}px`
     }
   });
 
@@ -161,7 +161,7 @@ function moveDodgerRight() {
     window.requestAnimationFrame(function() {
      const left = positionToInteger(DODGER.style.left)
      if (left<360) {
-       DODGER.style.left = `${left + 6}px`
+       DODGER.style.left = `${left + 4}px`
      }
    });
   
